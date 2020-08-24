64cc24a7917a8bb98111718d8d69671f8a9f3d34
Grace Powell <gracepowell145@gmail.com>
2020-08-24 09:16:16 -0500

    React-Drills-Incomplete

---
+++ app-2/src/App2.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import './App.css'
+
+class App2 extends Component {
+    constructor(){
+        super();
+
+        this.state={
+            name: ['Hunter', 'Avery','Evan',"Grace","Riley","Saqqara",'Chase']
+        }
+
+    }
+    render() {
+        let ourNames= this.state.name.map((element, index)=> {
+            return <h1 key={index}>{element}</h1>
+        })
+
+    return(
+        <div className="Siblings"> 
+        <h1>Siblings:</h1>
+            <h3>{ourNames}</h3>
+        </div>
+        
+    )
+}
+}
+
+export default App2;
